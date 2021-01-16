04c8d1fa74fd8e66eeaf96901f696eab2337c668
GHlacour <t.l.c.jansen@rug.nl>
2021-01-16 10:41:45 +0100

    Updated Linear Dichroism expample and added python file for plotting different techniques together

--- LH2/plot2D.py
+++ LH2/plot2D.py
@@ -27,6 +27,7 @@ w3 = np.reshape(Data[:,1],(w1Dim,w3Dim))
 
 PlotMap = np.reshape(Data[:,3],(w1Dim,w3Dim))
 
+fig=plt.figure()
 plt.contourf(w1,w3,PlotMap,20,cmap=plt.cm.bwr)
 plt.xlabel('$\omega_1$ [cm$^{-1}$]',fontsize=16)
 plt.ylabel('$\omega_3$ [cm$^{-1}$]',fontsize=16)
@@ -39,3 +40,38 @@ plt.plot([ wmin , wmax],[wmin,wmax],color='black')
 plt.xticks(np.arange(wmin,wmax+1,step),fontsize=12,rotation=0)
 plt.yticks(np.arange(wmin,wmax+1,step),fontsize=12,rotation=0)
 plt.show()
+fig.savefig('2Dpar.eps')
+
+Data = np.loadtxt('2D.per.dat')
+
+w1Dim=int(np.sqrt(len(Data[:,0])))
+w3Dim=w1Dim
+
+wmin=11000
+wmax=13000
+
+step=500
+mmax=np.max(np.abs(Data[:,3]))
+Data[:,3]=Data[:,3]/mmax
+Data[1,3]=1
+Data[2,3]=-1
+
+w1 = np.reshape(Data[:,0],(w1Dim,w3Dim))
+w3 = np.reshape(Data[:,1],(w1Dim,w3Dim))
+
+PlotMap = np.reshape(Data[:,3],(w1Dim,w3Dim))
+
+fig=plt.figure()
+plt.contourf(w1,w3,PlotMap,20,cmap=plt.cm.bwr)
+plt.xlabel('$\omega_1$ [cm$^{-1}$]',fontsize=16)
+plt.ylabel('$\omega_3$ [cm$^{-1}$]',fontsize=16)
+plt.gca().set_aspect('equal')
+
+plt.xlim(wmin,wmax)
+plt.ylim(wmin,wmax)
+
+plt.plot([ wmin , wmax],[wmin,wmax],color='black')
+plt.xticks(np.arange(wmin,wmax+1,step),fontsize=12,rotation=0)
+plt.yticks(np.arange(wmin,wmax+1,step),fontsize=12,rotation=0)
+plt.show()
+fig.savefig('2Dper.eps')
---
+++ LH2/plotAll.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['axes.linewidth'] = 2
+plt.rcParams['xtick.major.size']=8
+plt.rcParams['ytick.major.size']=8
+plt.rcParams['xtick.major.width']=2
+plt.rcParams['ytick.major.width']=2
+plt.rcParams['xtick.direction']='in'
+plt.rcParams['ytick.direction']='in'
+
+Data = np.loadtxt('Absorption.dat')
+DataLum = np.loadtxt('Luminescence.dat')
+DataLD = np.loadtxt('LD.dat')
+DataCD = np.loadtxt('CD.dat')
+
+fig=plt.figure()
+plt.xlim([11500,13000])
+plt.plot(Data[:,0],Data[:,1],'k')
+plt.plot(DataLum[:,0],DataLum[:,1]*6,'g')
+plt.plot(DataLD[:,0],DataLD[:,1],'r')
+plt.plot(DataCD[:,0],DataCD[:,1],'b')
+plt.legend(('Absorption','Luminescence','LD','CD'))
+plt.xlabel('$\omega$ [cm$^{-1}$]',fontsize=16)
+loc,labels=plt.yticks()
+plt.yticks(loc,[])
+plt.ylabel('Intensity [arb.u.]',fontsize=16)
+plt.axis('tight')
+plt.xlim([11000,13000])
+plt.show()
+fig.savefig('LH2_spectra.eps')
+
