bd404ef996f4b016bf5f0d984ad9d19403d5405b
kaizhong1027 <41535590+kaizhong1027@users.noreply.github.com>
2024-03-25 14:01:27 +0100

    Create submit_CG2DES.sh

---
+++ CG-2DES_LH2/submit_CG2DES.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time=1-24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=CG_2DES
+#SBATCH --mem=4000
+
+module load  foss
+/home3/p302934/NISE/NISE_2017/bin/NISE    inputCG2DES  #here you need to change the folder for NISE
